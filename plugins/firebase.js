32d7f6c8d5bea7b1dfa01fe58f3e24ca21d1b77e
ferretcode <49322226+FerretCode@users.noreply.github.com>
2022-03-08 22:25:47 -0800

    add barebones firebase plugin

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,1 +1,50 @@
-console.log("test");
+const { Plugin } = require("discord-trolling");
+const { MyCatLikesFirebaseServer } = require("my-cat-likes-firebase");
+
+/**
+ * A plugin for making Firebase easy to use as the DB for your bot
+ * @class
+ */
+module.exports.Plugin = class extends Plugin {
+  constructor() {
+    this.type = "client";
+
+    this.initializeFirebase = (configPath) => {
+      this.firebase = new MyCatLikesFirebaseServer({
+        firebaseCredentialsPath: configPath,
+      });
+
+      this.firebase.initialize();
+    };
+
+    this.createUser = (userId, initialData) => {
+      if (!this.firebase) this.initializeFirebase();
+
+      this.firebase
+        .createDoc(initialData, `users/${userId}`)
+        .then(() => {
+          return initialData;
+        })
+        .catch((err) => {
+          console.error(err);
+
+          return false;
+        });
+    };
+
+    this.updateUser = (userId, updatedData) => {
+      if (!this.firebase) this.initializeFirebase();
+
+      this.firebase
+        .updateDoc(updatedData, `users/${userId}`)
+        .then(() => {
+          return initialData;
+        })
+        .catch((err) => {
+          console.error(err);
+
+          return false;
+        });
+    };
+  }
+};
